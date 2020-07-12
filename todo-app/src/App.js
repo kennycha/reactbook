dcdf604a521375b8a2f7f0065ce959a0fdb46d55
kennycha <dudqn136@naver.com>
2020-07-12 22:49:51 +0900

    add todo func

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'react basic',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'component styling',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'make todo app',
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos([...todos, todo]);
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="a new thing to do" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="a new thing to do"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
